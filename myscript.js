e28f18f540a9d8b7b48e344afdf750c9f8f9cb30
FalseNerd <broken.dreams@gmx.com>
2024-12-18 18:44:22 +0100

    button changed

--- myscript.js
+++ myscript.js
@@ -4,3 +4,11 @@ function mostramessaggio() {
 
 // Aggiungi l'evento al bottone
 document.getElementById("myButton").addEventListener("click", mostramessaggio);
+
+function cambiaColoreSfondo() {
+  const form = document.getElementById("myForm");
+  form.style.backgroundColor = "lightblue"; // Cambia il colore a lightblue
+}
+
+// Aggiungi l'evento click al bottone
+document.getElementById("changeColorButton").addEventListener("click", cambiaColoreSfondo);
